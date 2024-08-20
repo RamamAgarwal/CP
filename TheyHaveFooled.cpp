e5082b9e85c0da2088d515b2baa221ca01576ab0
RamamAgarwal <ramamagarwal1234@gmail.com>
2024-08-20 15:29:54 +0530

    update

---
+++ TheyHaveFooled.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> lst = {10, 10, 8, 9, 8, 7, 7, 7, 7, 0, 9, 6, 8};
+    cout << lst[n] << endl;
+    return 0;
+}
